dd109f5bf9dc44129434b4ccad18a659d588af45
johnryan465 <johnryan465@gmail.com>
2020-03-21 16:42:39 +0000

    Added oracle

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/OxGroup14/prediction-market-oef.git
 git clone https://github.com/OxGroup14/prediction-market-aea.git
+git clone https://github.com/OxGroup14/prediction-market-oracle.git
 docker-compose build
