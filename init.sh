94c8e8a61caf499b9f198e6ce5544ffa767437a5
johnryan465 <johnryan465@gmail.com>
2020-03-23 15:04:43 +0000

    Updated docker compose

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 git clone https://github.com/OxGroup14/prediction-market-oef.git
 git clone https://github.com/OxGroup14/prediction-market-aea.git
 git clone https://github.com/OxGroup14/prediction-market-oracle.git
+git clone https://github.com/OxGroup14/prediction-market-ledger.git
 docker-compose build
